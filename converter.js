52bae354b4687a20db13a525fa7eb01a30809ed3
unknown <merjembajramovic8@gmail.com>
2020-05-03 01:13:46 +0200

    Base converter

--- converter.js
+++ converter.js
@@ -3,12 +3,25 @@
 /** Converter utility to convert data using specified datasets. */
 class Converter {
 
-    constructor(data) {
+    constructor(name, data) {
+        this.name = name;
         this.data = data;
     }
 
     onChange(event) {
             console.log(event);
+            const sourceUnit = event.target.getAttribute('id');
+            const sourceValue = event.target.value;
+            this.data.forEach((value, key) => {
+
+                if (key === sourceUnit) {
+                    return;
+                }
+
+                const result = this.convert(sourceUnit, key, sourceValue);
+                document.getElementById(key).value = result;
+
+            });
         }
         /**
          * @param source The source unit to convert from.
@@ -53,6 +66,7 @@ class Converter {
             let inputElement = document.createElement('input');
             inputElement.setAttribute('id', key);
             inputElement.setAttribute('type', 'number');
+            inputElement.addEventListener('change', (event) => this.onChange(event));
             inputElement.addEventListener('keyup', (event) => this.onChange(event));
             unitElement.appendChild(inputElement);
         });
@@ -61,9 +75,9 @@ class Converter {
     }
 }
 
-function onButtonPress(title, converter) {
+function onButtonPress(converter) {
     let titleElement = document.getElementById('header');
-    titleElement.innerHTML = event.target.innerText;
+    titleElement.innerHTML = converter.name;
 
     let interfaceElement = document.getElementById('units');
     interfaceElement.innerHTML = '';
@@ -72,48 +86,46 @@ function onButtonPress(title, converter) {
     interfaceElement.appendChild(unitElement);
 }
 
+const converters = [
+    new Converter('Length', new Map([
+        ['Millimeter', 1],
+        ['Centimeter', 10],
+        ['Meter', 1000],
+        ['Kilometer', 1000000],
+        ['Inch', 25.3999999999],
+        ['Feet', 304.7999999987],
+    ])),
+    new Converter('Weight', new Map([
+        ['Gram', 1],
+        ['Kilogram', 1000],
+        ['Pound', 453.5923699936],
+        ['Stone', 6350.2931799101]
+    ])),
+    new Converter('Area', new Map([
+        ['Square Millimeter', 1],
+        ['Square Centimeter', 100],
+        ['Square Inch', 645.1599999874],
+        ['Square Feet', 92903.0399981823],
+        ['Acre', 4046856422.320823],
+    ])),
+    new Converter('Volume', new Map([
+        ['Cubic Millimeter', 1],
+        ['Cubic Meter', 1000000000],
+        ['Cubic Dekameter', 1000000000000],
+        ['Cubic Inch', 16387.064],
+        ['Cubic Foot', 28316846.5917],
+        ['Cubic Yard', 764554857.9752],
+    ]))
+];
+
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("length").addEventListener("click", (event) => onButtonPress(event, length));
-    document.getElementById("mass").addEventListener("click", (event) => onButtonPress(event, mass));
-    document.getElementById("pressure").addEventListener("click", (event) => onButtonPress(event, null));
-    document.getElementById("speed").addEventListener("click", (event) => onButtonPress(event, null));
-    document.getElementById("surface").addEventListener("click", (event) => onButtonPress(event, surface));
-    document.getElementById("volume").addEventListener("click", (event) => onButtonPress(event, volume));
+    let menu = document.getElementById('menu');
+
+    converters.forEach((converter) => {
+        let menuElement = document.createElement('button');
+        menuElement.setAttribute('type', 'button');
+        menuElement.innerText = converter.name;
+        menuElement.addEventListener('click', () => onButtonPress(converter));
+        menu.appendChild(menuElement);
+    });
 });
-
-function onInput(event) {
-    console.log(event);
-}
-
-const length = new Converter(new Map([
-    ['Milimeter', 1],
-    ['Centimeter', 10],
-    ['Meter', 1000],
-    ['Kilometer', 1000000],
-    ['Inch', 25.3999999999],
-    ['Feet', 304.7999999987],
-]));
-
-const mass = new Converter(new Map([
-    ['Gram', 1],
-    ['Kilogram', 1000],
-    ['Pound', 453.5923699936],
-    ['Stone', 6350.2931799101]
-]));
-
-const surface = new Converter(new Map([
-    ['Square Millimeter', 1],
-    ['Square Centimeter', 100],
-    ['Square Inch', 645.1599999874],
-    ['Square Feet', 92903.0399981823],
-    ['Acre', 4046856422.320823],
-]));
-
-const volume = new Converter(new Map([
-    ['Cubic Millimeter', 1],
-    ['Cubic Meter', 1000000000],
-    ['Cubic Dekameter', 1000000000000],
-    ['Cubic Inch', 16387.064],
-    ['Cubic Foot', 28316846.5917],
-    ['Cubic Yard', 764554857.9752],
-]));
